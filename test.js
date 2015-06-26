5d452b020009a666da07e44b08f68322092316dd
Adam Buczynski <adambuczynski@gmail.com>
2015-06-26 13:28:52 +1200

    Add jshint, tests runner

---
+++ test.js
@@ -0,0 +1,28 @@
+
+/**
+ * Module dependencies
+ */
+var Jasmine = require('jasmine');
+var SpecReporter = require('jasmine-spec-reporter');
+var noop = function() {};
+
+//Initialize jasmine
+var jrunner = new Jasmine();
+
+//Set reporters
+jrunner.configureDefaultReporter({print: noop});
+jasmine.getEnv().addReporter(new SpecReporter());
+
+//Configure
+jrunner.loadConfig({
+  spec_dir: 'tests',
+  spec_files: [
+    '**/*.spec.js'
+  ],
+  helpers: [
+    'helpers/**/*.js'
+  ]
+});
+
+//Run tests
+jrunner.execute();
