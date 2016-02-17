ba614c51ef471c7a6d9218723f2583403520d20b
Adam Buczynski <adambuczynski@gmail.com>
2016-02-17 15:31:34 +1300

    Simplify travis file, use istanbul as dev dep

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -1,8 +1,18 @@
 'use strict';
 
+//Load dependencies
 let Promise = require('bluebird');
 let chai = require('chai');
+let dirtyChai = require('dirty-chai');
+let chaiAsPromised = require('chai-as-promised');
 
+//Enable should assertion style for usage with chai-as-promised
+chai.should();
+
+//Extend chai
+chai.use(dirtyChai);
+chai.use(chaiAsPromised);
+
+//Expose globals
 global.Promise = Promise;
 global.expect = chai.expect;
-global.assert = chai.assert;
