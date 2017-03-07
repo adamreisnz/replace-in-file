f99a27310d15652e3bede8b9aa9bfd8994659298
Dawson Botsford <dawsonbotsford@gmail.com>
2017-03-06 19:04:02 -0800

    Move changedFiles logging outside of if (#17)
    When the changed file was not found, there was no information from the cli which would let me know this. Because of that, an incorrect file path would be silently responded to with `"Replacing from with to"`.

--- bin/cli.js
+++ bin/cli.js
@@ -91,9 +91,8 @@ if (typeof argv.allowEmptyPaths !== 'undefined') {
 //Replace
 try {
   const changedFiles = replace.sync(options);
-  if (changedFiles.length > 0) {
-    console.log(chalk.green(changedFiles.length, 'file(s) were changed'));
-    if (argv.verbose) {
+  console.log(chalk.green(changedFiles.length, 'file(s) were changed'));
+    if (argv.verbose && (changedFiles.length > 0)) {
       changedFiles.forEach(file => console.log(chalk.grey('-', file)));
     }
   }
