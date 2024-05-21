9a77df24f3807fadf9fb3034da0b84278b6f4293
Mahdi Aghaei <mahd.aghaei@gmail.com>
2024-05-21 11:34:01 +0330

    feat: Add processor cb type with function overloads

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,11 @@
 
 declare module 'replace-in-file' {
-  export function replaceInFile(config: ReplaceInFileConfig): Promise<ReplaceResult[]>;
-  export function replaceInFile(config: ReplaceInFileConfig, cb: (error: Error, results: ReplaceResult[]) => void): void;
+  export function replaceInFile(config: ReplaceInFileConfig & { from?: never, to?: never, processor: ReplaceInFileConfig["processor"] }): Promise<ReplaceResult[]>;
+  export function replaceInFile(config: ReplaceInFileConfig & { from?: never, to?: never, processor: ReplaceInFileConfig["processor"] }, cb: (error: Error, results: ReplaceResult[]) => void): void;
+
+  export function replaceInFile(config: ReplaceInFileConfig & { from: ReplaceInFileConfig["from"], to: ReplaceInFileConfig["to"], processor?: never }): Promise<ReplaceResult[]>;
+  export function replaceInFile(config: ReplaceInFileConfig & { from: ReplaceInFileConfig["from"], to: ReplaceInFileConfig["to"], processor?: never }, cb: (error: Error, results: ReplaceResult[]) => void): void;
+
   export default replaceInFile;
 
   export namespace replaceInFile {
@@ -20,14 +24,15 @@ declare module 'replace-in-file' {
   export interface ReplaceInFileConfig {
     files: string | string[];
     ignore?: string | string[];
-    from: From | Array<From>;
-    to: To | Array<To>;
+    from?: From | Array<From>;
+    to?: To | Array<To>;
     countMatches?: boolean;
-    allowEmptyPaths?: boolean,
-    disableGlobs?: boolean,
-    encoding?: string,
-    dry?:boolean
-    glob?:object
+    allowEmptyPaths?: boolean;
+    disableGlobs?: boolean;
+    encoding?: string;
+    dry?: boolean;
+    glob?: object;
+    processor?: ProcessorCallback | Array<ProcessorCallback>;
   }
 
   export interface ReplaceResult {
@@ -40,3 +45,4 @@ declare module 'replace-in-file' {
 
 type FromCallback = (file: string) => string | RegExp | (RegExp | string)[];
 type ToCallback = (match: string, file: string) => string | string[];
+type ProcessorCallback = (input: string, file: string) => string;
